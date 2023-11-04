0aabcf27cf79bc427f681ac928fad7387bd1c07f
ChanceApp <49782599+ChanceApp@users.noreply.github.com>
2023-11-04 22:47:29 +0800

    Update demo01.py

--- 135-陈运智 广州/WEEK ONE/demo01.py
+++ 135-陈运智 广州/WEEK ONE/demo01.py
@@ -1,1 +1,21 @@
+# 输入两个数字相加
+num1 = input("请输入数字1：")
+num2 = input("请输入数字2：")
+sum=int(num1)+int(num2)
+print(sum)
+
+
+# 生成随机数，这个是百度的
+import random
+random.randint(1,10)
+
+# 九九乘法表格
+for i in range(9):
+    for j in range(9):
+        i
+        if i>=j:
+            s=(i+1)*(j+1)
+            print("%d"%(i+1),"*%d"%(j+1),"=%d"%s,end=" ")
+        else:
+            print()
 
